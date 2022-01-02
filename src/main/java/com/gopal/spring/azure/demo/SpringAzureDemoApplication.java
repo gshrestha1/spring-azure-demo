f4474370707fd9544c92e10d4ba67b79ecf1c6c4
Gopal Shrestha <gopal_shrestha@optum.com>
2022-01-02 17:10:25 -0500

    Minor updates to return message

--- src/main/java/com/gopal/spring/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/gopal/spring/azure/demo/SpringAzureDemoApplication.java
@@ -2,13 +2,15 @@ package com.gopal.spring.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringAzureDemoApplication {
 
+    @GetMapping("/message")
     public String message() {
 
-        return "Congrats!! Your Application is deployed in Azure";
+        return "Congrats!! Your Application is deployed in Azure successfully.";
     }
     public static void main(String[] args) {
         SpringApplication.run(SpringAzureDemoApplication.class, args);
